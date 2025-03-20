bdfd3c79e09c98be406a07ff8f0285fb9cb17056
KMRekky <maciek.szreder4444@gmail.comm>
2025-03-20 19:16:40 +0100

    Dodano treść do pliku Dokumentacja.py

---
+++ Dokumentacja.py
@@ -0,0 +1,12 @@
+# Opis funkcji wbudowanej 
+# zip() - łączy iterowalne obiekty w pary
+# Dokumentacja: https://docs.python.org/3/library/functions.html#zip
+
+# Opis modułu z biblioteki standardowej
+# math - funkcje matematyczne, np. sqrt()
+# Dokumentacja: https://docs.python.org/3/library/math.html
+
+# Opis wyjątku
+# ValueError - występuje przy błędnej wartości argumentu
+# Dokumentacja: https://docs.python.org/3/library/exceptions.html#ValueError
+print("Dokumentacja programu - przykładowa treść")
